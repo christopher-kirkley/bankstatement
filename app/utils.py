57d0f170654f3407c821af81123e15e924b482ca
Christopher Kirkley <ck@Christophers-MacBook-Pro.local>
2019-12-15 06:35:08 -0800

    Moved functions to seperate module.

---
+++ app/utils.py
@@ -0,0 +1,47 @@
+import csv
+from io import TextIOWrapper
+from itertools import islice
+from datetime import datetime
+
+from app.models import db, Statement
+
+
+def import_csv(csv_file, instance):
+    """
+    Function to import file and load into db.
+    Takes argument for csv_file and instance specific.
+    """
+
+    """Convert from file object."""
+    csv_file = TextIOWrapper(csv_file, encoding='utf-8')
+
+    """Delete all rows from current table."""
+    db.session.query(Statement).delete()
+    db.session.commit()
+
+    """Skip rows in header."""
+    tbl_reader = csv.reader(islice(csv_file, instance.skip, 100), delimiter=',')
+
+    """Iterate over file and add to database."""
+    for row in tbl_reader:
+        if float(row[2]) < 0:
+            """If negative value, add to debit column."""
+            db.session.add(Statement(date=datetime.strptime(row[0], '%m/%d/%Y'),
+                                     description=row[1],
+                                     debit=row[2]))
+            db.session.commit()
+        elif float(row[2]) > 0:
+            """If positive value, add to credit column."""
+            db.session.add(Statement(date=datetime.strptime(row[0], '%m/%d/%Y'),
+                                     description=row[1],
+                                     credit=row[2]))
+            db.session.commit()
+
+
+def write_csv():
+    """Function to export a CSV file from a query of table."""
+    with open('export.csv', 'w') as f:
+        out = csv.writer(f)
+        out.writerow(['date', 'description', 'debit', 'credit', 'category'])
+        for row in Statement.query.all():
+            out.writerow([row.date, row.description, row.debit, row.credit, row.category])
--- app/views.py
+++ app/views.py
@@ -1,16 +1,13 @@
 from app import app
-from pathlib import Path
 import csv
-from datetime import datetime
+from flask import render_template, request, redirect, url_for
+
+from sqlalchemy import func
 from app.models import Statement
 from app.models import db
 from app.models import ImportedCSV
-from itertools import islice
-from flask import render_template, request, redirect, url_for
 from .forms import MyForm
-from io import TextIOWrapper
-from sqlalchemy import func
-
+from app.utils import import_csv, write_csv
 
 @app.route('/')
 @app.route('/index')
@@ -23,37 +20,6 @@ def index():
 @app.route('/import', methods=['get', 'post'])
 def import_statement():
     """Route for importing."""
-
-    def import_csv(csv_file, instance):
-        """
-        Function to import file and load into db.
-        Takes argument for csv_file and instance specific.
-        """
-
-        """Convert from file object."""
-        csv_file = TextIOWrapper(csv_file, encoding='utf-8')
-
-        """Delete all rows from current table."""
-        db.session.query(Statement).delete()
-        db.session.commit()
-
-        """Skip rows in header."""
-        tbl_reader = csv.reader(islice(csv_file, instance.skip, 100), delimiter=',')
-
-        """Iterate over file and add to database."""
-        for row in tbl_reader:
-            if float(row[2]) < 0:
-                """If negative value, add to debit column."""
-                db.session.add(Statement(date=datetime.strptime(row[0], '%m/%d/%Y'),
-                                         description=row[1],
-                                         debit=row[2]))
-                db.session.commit()
-            elif float(row[2]) > 0:
-                """If positive value, add to credit column."""
-                db.session.add(Statement(date=datetime.strptime(row[0], '%m/%d/%Y'),
-                                         description=row[1],
-                                         credit=row[2]))
-                db.session.commit()
 
     """Create instances for use in importing."""
     bofa_csv = ImportedCSV('bank of america', 'row[0]', 'row[1]', 'row[2]', 7)
@@ -75,14 +41,6 @@ def import_statement():
 @app.route('/view', methods=['get', 'post'])
 def view_statement():
     """Route for viewing and updating imported statement."""
-
-    def write_csv():
-        """Function to export a CSV file from a query of table."""
-        with open('export.csv', 'w') as f:
-            out = csv.writer(f)
-            out.writerow(['date', 'description', 'debit', 'credit', 'category'])
-            for row in Statement.query.all():
-                out.writerow([row.date, row.description, row.debit, row.credit, row.category])
 
     tax_categories = ['Ask My Accountant', 'Office', 'Computer', 'Shipping Supplies', ]
 
